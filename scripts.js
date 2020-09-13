6c7a2b8d7527e4f56c2c49b3de51d5697a6b4953
flor2801 <florenciasarria1@hotmail.com>
2020-09-13 20:12:22 -0300

    Agregue Logo ADA / Agregue JS para filtros / Cree carrito-menu / Agregue img fondo / Position lupa busqueda

--- scripts.js
+++ scripts.js
@@ -1,107 +1,110 @@
-const cards = document.getElementsByClassName('tarjeta')
-const filtroRating = document.getElementsByClassName('puntaje-review')
-const filtroNombre = document.querySelector("#search")
-const titulos = document.querySelectorAll(".title")
+const filtroRating = document.getElementsByClassName("review");
+const filtroNombre = document.getElementById("search");
+const tarjetas = document.getElementsByClassName("tarjeta");
+const filtroCategoria = document.getElementsByClassName("tipo");
+const limpiar = document.getElementById("limpieza");
 
-
-
-///////////////// BUSQUEDA CON FILTRO DE TEXTO //////////////////////////////////
-
+///////////////// BUSQUEDA CON FILTRO DE TEXTO /////////////////////////////
 
 filtroNombre.oninput = () => {
-    const titulo = titulos.textContent
-    const input = filtroNombre.value
-
-if (titulo.includes(input)) {
-    console.log ("si")
-}
-else {
-    console.log ("no")
-}
-
-
-}
-
-/*
-// Cuando se enscriba algo en el input //
-filtroNombre.oninput = () => {
-    // recorro una a una las tarjetas //
-    for (let tarjeta of cards) {
-    // me fijo el nombre de la tarjeta que busco el usuario
-    const titulo =  tarjeta.dataset.nombre
-    const busqueda = filtroNombre.value
-       // si el titulo incluye lo que busco el usuario
-    if (titulo.includes(busqueda)) {
-        //le quito la clase hidden a la tarjeta
-        tarjeta.classList.remove('hidden');
-    }  else {
-        tarjeta.classList.add('hidden');
+  for (let card of tarjetas) {
+    const input = filtroNombre.value;
+    const nombre = card.dataset.nombre;
+    if (nombre.includes(input)) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
     }
-
-    
-}
-
+  }
 };
 
+///////////////// BUSQUEDA POR CATEGORIAS /////////////////////////////
 
+for (let checkbox of filtroCategoria) {
+  checkbox.onclick = () => {
+    for (let tarjeta of tarjetas) {
+      if (checkbox.checked) {
+        const tipo = tarjeta.dataset.categoria;
 
-///////////////// BUSQUEDA CON CHECKBOX  ///////////////////////////////
+        if (checkbox.value === tipo) {
+          tarjeta.classList.remove("hidden");
+        } else {
+          tarjeta.classList.add("hidden");
+        }
+      } else {
+        tarjeta.classList.remove("hidden");
+      }
+    }
+  };
+}
+
+///////////////// BUSQUEDA POR PUNTAJE  ///////////////////////////////
 
 for (let checkbox of filtroRating) {
+  // si hacen clic en algun checkbox
 
-// si hacen clic en algun checkbox 
-
-checkbox.onclick = () => {
+  checkbox.onclick = () => {
     //recorro una a una las tarjetas
-for (let tarjeta of cards) {
-
-    // si el chackbox esta seleccionado
-    if (checkbox.checked) {
+    for (let tarjeta of tarjetas) {
+      // si el chackbox esta seleccionado
+      if (checkbox.checked) {
         const rating = tarjeta.dataset.rating;
 
         // veo si el rating es igual al checkbox
         if (checkbox.value === rating) {
-            tarjeta.classList.remove('hidden');
-            } 
-            else { 
-                tarjeta.classList.add('hidden');
-            }
-
-
-            //si el checkbox no esta seleccionado 
+          tarjeta.classList.remove("hidden");
+        } else {
+          tarjeta.classList.add("hidden");
         }
-        else {
-            tarjeta.classList.remove('hidden');
-        }
+
+        //si el checkbox no esta seleccionado
+      } else {
+        tarjeta.classList.remove("hidden");
+      }
     }
 
-
-// se cierra for (let tarjeta of tarjetas) 
+    // se cierra for (let tarjeta of tarjetas)
+  };
+  // se cierra checkbox.onclic
 }
-// se cierra checkbox.onclic
-};
 
-
-/////////////////
-
-const campoTexto = document.getElementById('search')
-const puntaje = document.getElementsByClassName('puntaje-review')
-const limpiar = document.getElementById('limpieza')
-
-
-console.log (puntaje)
-
-
+//////////////////////////   LIMPIAR   /////////////////////////////////
+/*
 limpiar.onclick = () => {
 
-for (let punto of puntaje) {
+for (let punto of filtroRating) {
      punto.checked = false;
+     if (punto.checked = false) {
+        tarjeta.classList.remove('hidden');
+        } 
+    }
+
+for (let tipo of filtroCategoria) {
+     tipo.checked = false;
+     if (tipo.checked = false) {
+        tarjeta.classList.remove('hidden');
+        } 
 }
 
 campoTexto.value = "  ";
 
 
-}
 
+}
 */
 
+//////////////////////////  ABRIR CARRITO   ////////////////////////////////
+
+const carrito = document.getElementById("abrir-carrito");
+const menu = document.getElementById("menu-compra");
+const cerrar = document.getElementById("cerrar");
+const overlay = document.getElementById("overlay");
+
+carrito.onclick = () => {
+  overlay.classList.remove("hidden");
+  menu.classList.remove("hidden");
+};
+
+cerrar.onclick = () => {
+  overlay.classList.add("hidden");
+};
