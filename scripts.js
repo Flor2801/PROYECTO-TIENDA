045e4ec070cbb5d65c6dde2d14d215d9856d0f8c
flor2801 <florenciasarria1@hotmail.com>
2020-10-04 17:44:33 -0300

    Filtros funcionando OK

--- scripts.js
+++ scripts.js
@@ -1,17 +1,254 @@
+const filtroCategoria = document.getElementsByClassName("tipo");
 const filtroRating = document.getElementsByClassName("review");
 const filtroNombre = document.getElementById("search");
 const tarjetas = document.getElementsByClassName("tarjeta");
-const filtroCategoria = document.getElementsByClassName("tipo");
-const campoTexto = document.getElementById("search");
-const limpiar = document.getElementById("limpieza")
+const limpiar = document.getElementById("limpieza");
+
+console.log(tarjetas)
 
 
-///////////////// BUSQUEDA CON FILTRO DE TEXTO /////////////////////////////
+ 
+////////////////// FUNCIONES SEGUN TIPO DE BUSQUEDA  ///////////////////
+
 
 filtroNombre.oninput = () => {
+  filtrarTarjetas ()
+}
+
+
+for (let tipo of filtroCategoria) { 
+  tipo.oninput = () => {
+    filtrarTarjetas ()
+  }
+}
+
+
+for (let punto of filtroRating) {
+  punto.oninput = () => {
+    filtrarTarjetas ()
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+
+const filtrarTarjetas = () => {
+  for (let tarjeta of tarjetas) {
+    if (pasaFiltros(tarjeta)) {
+      mostrarTarjetas(tarjeta)
+    }
+    else {
+      ocultarTarjetas(tarjeta)
+    }
+  }
+}
+
+
+
+const mostrarTarjetas = (tarjeta) => {
+  return tarjeta.classList.remove("hidden")
+}
+
+const ocultarTarjetas = (tarjeta) => {
+  return tarjeta.classList.add("hidden")
+}
+
+
+
+
+const pasaFiltros = (tarjeta) => {
+
+  if (filtroInputEscrito(tarjeta)== true && filtroProductos(tarjeta) == true && filtroEstrellas(tarjeta)== true) {
+    return true
+}
+else {
+    return false
+}
+
+}
+
+
+
+////////////////// FILTRO INPUT /////////////////
+
+const filtroInputEscrito = (tarjeta) => {
+  if (hayAlgoEscritoEnInput()) {
+    return coincideBusquedaInputConTarjeta(tarjeta)
+}
+else {
+    return true
+}
+}
+
+const hayAlgoEscritoEnInput = () => {
+  return Boolean(filtroNombre.value)
+}
+
+const coincideBusquedaInputConTarjeta = (tarjeta) => {
+  const nombreTarjeta = tarjeta.dataset.nombre
+  const busquedaUsuario = filtroNombre.value.toLowerCase()
+  
+  if (nombreTarjeta.includes(busquedaUsuario)) {
+      return true
+  }
+  else {
+      return false
+  }
+}
+
+
+////////////////// FILTRO CATEGORIA /////////////////
+
+const filtroProductos = (tarjeta) => {
+  
+  if (hayAlgunTipoChequeado()) {
+    return coincideTipoConTarjeta(tarjeta)
+}
+else {
+    return true
+}
+}
+
+const hayAlgunTipoChequeado = () => {
+  for (let tipo of filtroCategoria) {
+      if (tipo.checked) {
+          return true
+      }
+  }
+  return false
+}
+
+const coincideTipoConTarjeta = (tarjeta) => {
+  for (let tipo of filtroCategoria) {
+      if (tipo.value === tarjeta.dataset.categoria && tipo.checked) {
+          return true
+      }
+      if (tipo.value === "todos" && tipo.checked) {
+          return true
+      }
+  }
+  return false
+}
+
+
+
+////////////////// FILTRO PUNTAJE /////////////////
+
+
+const filtroEstrellas = (tarjeta) => {
+  
+  if (hayAlgunPuntoSeleccionado()) {
+    return coincidePuntoConTarjeta(tarjeta)
+}
+else {
+    return true
+}
+}
+
+const hayAlgunPuntoSeleccionado = () => {
+  for (let punto of filtroRating) {
+      if (punto.checked) {
+          return true
+      }
+  }
+  return false
+}
+
+const coincidePuntoConTarjeta = (tarjeta) => {
+  for (let punto of filtroRating) {
+      if (punto.value === tarjeta.dataset.rating && punto.checked) {
+          return true
+      }
+     
+  }
+  return false
+}
+
+
+
+/////////////////////////////////   LIMPIAR   /////////////////////////////////
+
+
+limpiar.onclick = () => {
+
+  for (cards of tarjetas) {
+  cards.classList.remove('hidden')
+  }
+
+for (punto of filtroRating) {
+     punto.checked = false;
+     if (punto.checked = false) {
+        tarjeta.classList.remove('hidden');
+        } 
+    }
+
+for (tipo of filtroCategoria) {
+     tipo.checked = false;
+     if (tipo.checked = false) {
+        tarjeta.classList.remove('hidden');
+        } 
+}
+
+filtroNombre.value = "";
+
+}
+
+
+//////////////////////////  ABRIR CARRITO   ////////////////////////////////
+
+const carrito = document.getElementById("abrir-carrito");
+const menu = document.getElementById("menu-compra");
+const cerrar = document.getElementById("cerrar");
+const overlay = document.getElementById("overlay");
+const body = document.getElementsByTagName("body");
+
+
+carrito.onclick = () => {
+  overlay.classList.remove('hidden');
+  overlay.classList.add('no-scroll');
+};
+
+
+cerrar.onclick = () => {
+  overlay.classList.add('hidden');
+};
+
+//////////////////////////////// MODAL CHECKOUT ///////////////////////////
+
+
+const botonSeguir = document.getElementsByClassName("seguir")
+const botonFinalizar = document.getElementsByClassName("finalizar")
+const modalCheckout = document.getElementsByClassName("modal-compra")
+
+console.log(botonSeguir)
+console.log(botonFinalizar)
+console.log(modalCheckout)
+
+botonSeguir.onclick = () => {
+  modalCheckout.classList.add('hidden');
+}
+
+botonFinalizar.onclick = () => {
+  modalCheckout.classList.add('hidden');
+}
+
+
+
+
+
+
+////////////////// /////////////  /////////////////////  //////////////////
+///////////////// BUSQUEDA CON FILTRO DE TEXTO /////////////////////////////
+/*
+
+filtroNombre.oninput = () => {
+
   for (let card of tarjetas) {
-    const input = filtroNombre.value;
+    const input = filtroNombre.value.toLowerCase();
     const nombre = card.dataset.nombre;
+
     if (nombre.includes(input)) {
       card.classList.remove("hidden");
     } else {
@@ -21,6 +258,7 @@ filtroNombre.oninput = () => {
 };
 
 ///////////////// BUSQUEDA POR CATEGORIAS /////////////////////////////
+
 
 for (let checkbox of filtroCategoria) {
   checkbox.onclick = () => {
@@ -40,12 +278,64 @@ for (let checkbox of filtroCategoria) {
   };
 }
 
-///////////////// BUSQUEDA POR PUNTAJE  ///////////////////////////////
+
+////////////////////////// BUSQUEDA POR PUNTAJE  ///////////////////////////////
+
+
 
 for (let checkbox of filtroRating) {
   // si hacen clic en algun checkbox
 
-  checkbox.onclick = () => {
+checkbox.onclick = () => {
+    //recorro una a una las tarjetas
+    for (let tarjeta of tarjetas) {
+      tarjeta.classList.add("hidden"); 
+
+      if (checkbox.checked) {
+
+      for (let tarj2 of tarjetas) {
+        if (tarj2.checked) {
+          if (tarj2.value === tarjeta.dataset.rating) {
+            tarjeta.classList.remove("hidden");
+          } else if (tarj2.value==="") {
+            tarjeta.classList.remove("hidden");
+          }
+    }
+ }
+
+}
+
+}
+
+if (checkbox.checked) {
+  const rating = tarjeta.dataset.rating;
+
+// veo si el rating es igual al checkbox
+  if (checkbox.value === rating) {
+    tarjeta.classList.remove("hidden");
+  } else {
+    tarjeta.classList.add("hidden");
+  }
+
+//si el checkbox no esta seleccionado
+} else {
+  tarjeta.classList.remove("hidden");
+}
+}
+
+// se cierra for (let tarjeta of tarjetas)
+};
+// se cierra checkbox.onclic
+}
+
+
+
+/*
+
+for (let checkbox of filtroRating) {
+  // si hacen clic en algun checkbox
+
+checkbox.onclick = () => {
     //recorro una a una las tarjetas
     for (let tarjeta of tarjetas) {
       // si el chackbox esta seleccionado
@@ -69,46 +359,4 @@ for (let checkbox of filtroRating) {
   };
   // se cierra checkbox.onclic
 }
-
-
-
-//////////////////////////   LIMPIAR   /////////////////////////////////
-
-limpiar.onclick = () => {
-
-for (let punto of filtroRating) {
-     punto.checked = false;
-     if (punto.checked = false) {
-        tarjeta.classList.remove('hidden');
-        } 
-    }
-
-for (let tipo of filtroCategoria) {
-     tipo.checked = false;
-     if (tipo.checked = false) {
-        tarjeta.classList.remove('hidden');
-        } 
-}
-
-campoTexto.value = "";
-
-}
-
-
-//////////////////////////  ABRIR CARRITO   ////////////////////////////////
-
-const carrito = document.getElementById("abrir-carrito");
-const menu = document.getElementById("menu-compra");
-const cerrar = document.getElementById("cerrar");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName('body');
-
-carrito.onclick = () => {
-  overlay.classList.remove("hidden");
-  body.classList.add("no-scroll");
-  
-};
-
-cerrar.onclick = () => {
-  overlay.classList.add("hidden");
-};
+*/
